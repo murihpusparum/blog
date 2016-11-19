c2783c2fb88cbf3e4f95fc68ff304605833dbbac
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2016-11-19 23:28:34 +0700

    remove fastclick

--- _js/app.js
+++ _js/app.js
@@ -1,16 +1,8 @@
-var Fastclick = require("fastclick");
+document.getElementById('navbar').addEventListener('click', function(e) {
 
-if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-	Fastclick(document.body);
-    }, false);
-
-    document.getElementById('navbar').addEventListener('click', function(e) {
-
-	if (e.target && e.target.classList.contains('js-navbar-menu-btn')) {
-	    document.getElementsByClassName('js-navbar-menu')[0].classList.toggle('is-active');
-	}
-	
-    });    
-}
+    if (e.target && e.target.classList.contains('js-navbar-menu-btn')) {
+	document.getElementsByClassName('js-navbar-menu')[0].classList.toggle('is-active');
+    }
+    
+});    
 
