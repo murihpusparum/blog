91ca413ec0b5b80c6182b0cb883a71fadc9e15a4
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2016-11-19 22:35:56 +0700

    fix bug and add fastclick

--- _js/app.js
+++ _js/app.js
@@ -1,7 +1,15 @@
-document.getElementById('navbar').addEventListener('click', function(e) {
+var Fastclick = require("fastclick");
 
-    if (e.target && e.target.classList.contains('js-navbar-menu-btn')) {
-	document.getElementsByClassName('js-navbar-menu')[0].classList.toggle('is-active');
-    }
-    
-});
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+	Fastclick(document.body);
+    }, false);
+
+    document.getElementById('navbar').addEventListener('click', function(e) {
+
+	if (e.target && e.target.classList.contains('js-navbar-menu-btn')) {
+	    document.getElementsByClassName('js-navbar-menu')[0].classList.toggle('is-active');
+	}
+	
+    });    
+}
